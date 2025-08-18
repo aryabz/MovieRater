341df9755522658e5e009a298af23ab439d9b7c6
ariabarzegar <ariabarzegar008@gmail.com>
2025-08-19 03:15:18 +0330

    Create the base main component and the remaining components

--- src/App_2.js
+++ src/App_2.js
@@ -4,6 +4,7 @@ export default function App() {
     return (
         <div>
             <Header />
+            <Main />
         </div>
     )
 }
@@ -13,20 +14,15 @@ function Header() {
         <div className="header">
 
             <div className="box-movies-header disply-flex ">
-                <div >
-                    <img src="https://img.uptvs.com/uploads/2020/07/Mr-Robot-s4-poster-207x290.jpg" alt="" className="photo-title" />
-                </div>
+
+                <Photo url={"https://img.uptvs.com/uploads/2020/07/Mr-Robot-s4-poster-207x290.jpg"} borderRadius={16} />
                 <div className="tozeh disply-flex " style={{ width: "100%" }}>
                     <h1>Mr Robot</h1>
                     <div className="wrpper-border disply-flex justf-between align-center">
-                        <h2 className=" disply-flex align-center">
-                            <span>
-                                <img src="/imdb-icon.svg" alt="لوگوی IMDb" width="40" height="40" style={{ margin: "0 0.5rem" }} />
-                            </span>
-                            8.5 </h2>
-                        <h2>United States, Germany</h2>
-                        <h2>115 min</h2>
-                        <h2> 2015</h2>
+                        <ImdbRot roting={8.5} />
+                        <Country nameCountry={"United States, Germany"} />
+                        <Minit min={115} />
+                        <Year movieYear={2015} />
                     </div>
                     <div className="About-creators">
                         <SelectStar />
@@ -39,3 +35,71 @@ function Header() {
     )
 }
 
+function Photo({ url, borderRadius }) {
+    return (
+        <img src={url} alt="" className="photo-title" style={{ borderRadius }} />
+    )
+}
+
+function ImdbRot({ roting }) {
+    return (<h2 className=" disply-flex align-center">
+        <span>
+            <img src="/imdb-icon.svg" alt="لوگوی IMDb" width="40" height="40" style={{ margin: "0 0.5rem" }} />
+        </span>
+        {roting}
+    </h2>)
+}
+
+function Country({ nameCountry }) {
+    return (
+        <h2>{nameCountry}</h2>
+    )
+}
+
+function Minit({ min }) {
+    return (
+        <h2>{min} min</h2>
+    )
+}
+
+function Year({ movieYear }) {
+    return (
+        <h2> {movieYear}</h2>
+    )
+}
+
+
+function Main() {
+    const moviesList = ["a", "b", "c", "d", "e", "f", "h    "]
+    return (
+        <div>
+            <SendData />
+            <ItemMovies moviesList={moviesList} />
+        </div>
+    )
+}
+function SendData() {
+    return (
+        <form className="search-box-movie">
+            <input type="text" className="search-box-movie__input" />
+            <button className="search-box-movie__button">🔎</button>
+        </form>
+    )
+}
+
+function ItemMovies({ moviesList }) {
+    return (
+        <div className="wrapper-item disply-flex justf-around">
+            {moviesList.map(movie => <Item movie={movie} />)}
+
+        </div>
+    )
+}
+
+function Item({ movie }) {
+    return (
+        <div className="item justf-center disply-flex justf-center">
+            <img src="https://img.uptvs.com/uploads/2020/07/Mr-Robot-s4-poster-207x290.jpg" alt="" width={"150px"} />
+        </div>
+    )
+}
