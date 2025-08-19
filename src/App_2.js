6b093acd0e0a4037f4433a68244e0d13e7602434
ariabarzegar <ariabarzegar008@gmail.com>
2025-08-19 22:51:19 +0330

    Add useEffect usestate Api functionality in the main component for the movie list

--- src/App_2.js
+++ src/App_2.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import SelectStar from "./selectStar";
 
+
+const Key = "ea397b57"
 export default function App() {
     return (
         <div>
@@ -20,9 +23,12 @@ function Header() {
                     <h1>Mr Robot</h1>
                     <div className="wrpper-border disply-flex justf-between align-center">
                         <ImdbRot roting={8.5} />
-                        <Country nameCountry={"United States, Germany"} />
-                        <Minit min={115} />
-                        <Year movieYear={2015} />
+                        <h2>United States, Germany</h2>
+
+                        <h2>115 min</h2>
+
+                        <h2> 2015</h2>
+
                     </div>
                     <div className="About-creators">
                         <SelectStar />
@@ -50,38 +56,74 @@ function ImdbRot({ roting }) {
     </h2>)
 }
 
-function Country({ nameCountry }) {
-    return (
-        <h2>{nameCountry}</h2>
-    )
-}
-
-function Minit({ min }) {
-    return (
-        <h2>{min} min</h2>
-    )
-}
-
-function Year({ movieYear }) {
-    return (
-        <h2> {movieYear}</h2>
-    )
-}
-
 
 function Main() {
-    const moviesList = ["a", "b", "c", "d", "e", "f", "h    "]
+    // const moviesList = ["a", "b", "c", "d", "e", "f", "h    "]
+    const [query, setQuery] = useState("Robot");
+    const [dataMovies, SetdataMovies] = useState([])
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState()
+
+
+
+
+    useEffect(function () {
+        const controller = new AbortController()
+        async function getData() {
+            setError("")
+            setLoading(true)
+            try {
+                const res = await fetch(`http://www.omdbapi.com/?apikey=${Key}&s=${query}`, { signal: controller.signal });
+                console.log(res);
+                if (!res.ok) throw new Error("cant conecting server");
+                const Data = await res.json();
+                if (Data.Response === "False") throw new Error("not fund movies")
+                console.log(Data);
+                console.log(Data.Search);
+                SetdataMovies(Data.Search);
+
+            } catch (err) {
+                if (err.name !== "AbortError") {
+                    setError(err.message)
+                }
+            }
+            setLoading(false)
+        }
+        getData()
+
+        return function () {
+            controller.abort();
+        }
+    }, [query])
+
     return (
         <div>
-            <SendData />
-            <ItemMovies moviesList={moviesList} />
+            <SendData query={query} setQuery={setQuery} />
+            {loading && <LoadingMs />}
+            {error && <ErrorMovies error={error} />}
+            {!error && !loading && <ItemMovies moviesList={dataMovies} />}
         </div>
     )
 }
-function SendData() {
+
+function ErrorMovies({ error }) {
+    return (
+        <h1 style={{ color: "#fff", textAlign: "center" }}>
+            {error}
+        </h1>
+    )
+}
+
+function LoadingMs() {
+    return (
+        <h1 style={{ color: "#fff", textAlign: "center" }}>Loading...</h1>
+    )
+}
+
+function SendData({ query, setQuery }) {
     return (
         <form className="search-box-movie">
-            <input type="text" className="search-box-movie__input" />
+            <input type="text" className="search-box-movie__input" value={query} onChange={e => setQuery(e.target.value)} />
             <button className="search-box-movie__button">🔎</button>
         </form>
     )
@@ -90,16 +132,17 @@ function SendData() {
 function ItemMovies({ moviesList }) {
     return (
         <div className="wrapper-item disply-flex justf-around">
-            {moviesList.map(movie => <Item movie={movie} />)}
-
+            {moviesList.map(movie => <Item movie={movie} key={movie.imdbID} />)}
         </div>
     )
 }
 
 function Item({ movie }) {
     return (
-        <div className="item justf-center disply-flex justf-center">
-            <img src="https://img.uptvs.com/uploads/2020/07/Mr-Robot-s4-poster-207x290.jpg" alt="" width={"150px"} />
+        <div className="item  disply-flex  align-center">
+            <img src={movie.Poster} alt="" width={"150px"} height={"200px"} style={{ borderRadius: "inherit" }} />
+            <h4 style={{ margin: "1rem 0 0 0" }}>{movie.Title}</h4>
+            <p>{movie.Year}</p>
         </div>
     )
 }
